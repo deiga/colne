3f068020f9500e3cb4e0365f83654454cb45f21b
Timo Sand <timo.sand@iki.fi>
2018-02-16 10:42:48 +0200

    Updates Hapi usage

--- index.js
+++ index.js
@@ -3,21 +3,20 @@
 const Hapi = require('hapi');
 const unirest = require('unirest');
 
-const server = new Hapi.Server();
-server.connection({ 
-  port: process.env.PORT, 
-  routes: { 
+const server = new Hapi.Server({
+  port: process.env.PORT,
+  routes: {
     cors: {
       additionalHeaders: ['Accept-Language']
-    } 
+    }
   }
 });
 
 server.route({
   method: 'GET',
   path: '/',
-  handler: function (request, reply) {
-    reply('Hello, world!');
+  handler: function (request, h) {
+    return 'Hello, world!';
   }
 });
 
@@ -30,23 +29,26 @@ server.route({
       parse: true
     }
   },
-  handler: function (request, reply) {
+  handler: function (request, h) {
     console.log(request.payload);
     unirest.post(`https://maker.ifttt.com/trigger/${process.env.EVENT_NAME}/with/key/${process.env.MAKER_KEY}`)
     .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
     .send({ "value1": request.payload.words , "value2": request.payload.body, "value3": request.payload.date })
     .end(function (response) {
       console.log(response.body);
-      reply(response.body);
+      return response.body;
     });
   }
 });
 
-
-server.start((err) => {
-  
-  if (err) {
-    throw err;
+async function connect() {
+  try {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
   }
-  console.log(`Server running at: ${server.info.uri}`);
-});
+  catch (err) {
+    console.log(err);
+  }
+}
+
+connect()
